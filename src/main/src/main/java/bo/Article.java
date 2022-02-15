138b5054566a9e3cafd5454f8012e82816521e49
MAT <MAT@10.0.2.15>
2022-02-15 16:18:36 +0100

    création class Article

---
+++ src/main/src/main/java/bo/Article.java
@@ -0,0 +1,112 @@
+package bo;
+
+import java.util.Date;
+
+public class Article {
+
+	private int noArticle, prixInitial, prixVente, prixInit, noUtilisateur, noCategorie;
+	private Date dateDebut, dateFin;
+	private String nomArticle, description;
+
+	public Article () {
+
+	}
+	
+	public Article (int prixInitialArt, int prixInitArt, int noCategorieArt, 
+			String nomArticleVente, String descriptionArticle,
+			Date dateDebutVente, Date dateFinVente ) {
+
+		this.prixInitial = prixInitialArt;
+		this.dateDebut = dateDebutVente;
+		this.dateFin = dateFinVente;
+		this.prixInit = prixInitArt;
+		this.noCategorie = noCategorieArt;
+		this.nomArticle = nomArticleVente;
+		this.description = descriptionArticle;
+
+	}
+
+	public int getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(int noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public int getPrixInitial() {
+		return prixInitial;
+	}
+
+	public void setPrixInitial(int prixInitial) {
+		this.prixInitial = prixInitial;
+	}
+
+	public int getPrixVente() {
+		return prixVente;
+	}
+
+	public void setPrixVente(int prixVente) {
+		this.prixVente = prixVente;
+	}
+
+	public int getPrixInit() {
+		return prixInit;
+	}
+
+	public void setPrixInit(int prixInit) {
+		this.prixInit = prixInit;
+	}
+
+	public int getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(int noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public int getNoCategorie() {
+		return noCategorie;
+	}
+
+	public void setNoCategorie(int noCategorie) {
+		this.noCategorie = noCategorie;
+	}
+
+	public Date getDateDebut() {
+		return dateDebut;
+	}
+
+	public void setDateDebut(Date dateDebut) {
+		this.dateDebut = dateDebut;
+	}
+
+	public Date getDateFin() {
+		return dateFin;
+	}
+
+	public void setDateFin(Date dateFin) {
+		this.dateFin = dateFin;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	
+
+	
+
+}
