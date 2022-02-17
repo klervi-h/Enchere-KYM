f7c57f9731a39f4d793fea723d50977a855cd9be
MAT <MAT@10.0.2.15>
2022-02-17 17:59:45 +0100

    création servlet ProfilVendeur + jsp profilVendeur ok

---
+++ src/main/java/encheres/ihm_webcontroller/ProfilVendeur.java
@@ -0,0 +1,36 @@
+package encheres.ihm_webcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ProfilVendeur
+ */
+@WebServlet("/ProfilVendeur")
+public class ProfilVendeur extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/profilVendeur.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/profilVendeur.jsp");
+		rd.forward(request, response);
+	}
+
+}
