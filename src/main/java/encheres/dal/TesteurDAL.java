eb547d7e35f905220ac5ddad03dc8d15ae2c7711
klerv <klerv@192.168.0.26>
2022-02-22 08:52:32 +0100

    testeur dal update

--- src/main/java/encheres/dal/TesteurDAL.java
+++ src/main/java/encheres/dal/TesteurDAL.java
@@ -13,6 +13,10 @@ public class TesteurDAL {
 		Utilisateur u = utilisateurDAO.selectById(3);
 		System.out.println(u.toString());
 		
+		Utilisateur u2 = new Utilisateur(u.getNoUtilisateur(),u.getPseudo(),u.getNom(),u.getPrenom(),u.getEmail(),u.getTelephone(),u.getAdresse().getRue(),44000,u.getAdresse().getVille(),u.getMotDePasse(),30);
+		
+		utilisateurDAO.update(u2);
+		
 		ArticleDAO articleDAO = DAOFactory.getArticleDao();
 		System.out.println("recuperation d'un article... ");
 		try {
