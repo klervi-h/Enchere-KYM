f282cd8fd7c7a0f177d75cd345ded722bf9fbe37
MAT <MAT@10.0.2.15>
2022-02-23 17:25:42 +0100

    creation encheres bll bo dal

--- src/main/java/encheres/dal/DAOFactory.java
+++ src/main/java/encheres/dal/DAOFactory.java
@@ -3,29 +3,40 @@ package encheres.dal;
 import java.lang.reflect.InvocationTargetException;
 
 public class DAOFactory {
-	
+
 
 	public static UtilisateurDAO getUtilisateurDao() {
 
-			try {
-				return (UtilisateurDAO) Class.forName("encheres.dal.jdbc.UtilisateurDaoJdbcImpl").getDeclaredConstructor().newInstance();
-			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException | SecurityException
-					| ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			return null;
+		try {
+			return (UtilisateurDAO) Class.forName("encheres.dal.jdbc.UtilisateurDaoJdbcImpl").getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException
+				| ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 
 	public static ArticleDAO getArticleDao() {
 
-			try {
-				return (ArticleDAO) Class.forName("encheres.dal.jdbc.ArticleDaoJdbcImpl").getDeclaredConstructor().newInstance();
-			} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
-					| InvocationTargetException | NoSuchMethodException | SecurityException
-					| ClassNotFoundException e) {
-				e.printStackTrace();
-			}
-			return null;
+		try {
+			return (ArticleDAO) Class.forName("encheres.dal.jdbc.ArticleDaoJdbcImpl").getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException
+				| ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
+	}
+
+	public static EncheresDAO getEnchereDAO() {
+		try {
+			return (EncheresDAO) Class.forName("encheres.dal.jdbc.EncheresDaoJdbcImpl").getDeclaredConstructor().newInstance();
+		} catch (InstantiationException | IllegalAccessException | IllegalArgumentException
+				| InvocationTargetException | NoSuchMethodException | SecurityException
+				| ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		return null;
 	}
 }
--- src/main/java/encheres/dal/jdbc/EncheresDaoJdbcImpl.java
+++ src/main/java/encheres/dal/jdbc/EncheresDaoJdbcImpl.java
@@ -1,9 +1,82 @@
 package encheres.dal.jdbc;
 
-public class EncheresDaoJdbcImpl {
+import java.sql.Connection;
+import java.sql.Date;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-	public EncheresDaoJdbcImpl() {
-		// TODO Auto-generated constructor stub
+import encheres.buisness.bo.Encheres;
+import encheres.dal.DALException;
+import encheres.dal.EncheresDAO;
+
+public class EncheresDaoJdbcImpl implements EncheresDAO {
+	
+	private static final String sqlSelectById ="select * from ENCHERES where no_article = ?";
+	private static final String sqlInsert ="insert into ENCHERES (no_utilisateur, no_article, date_enchere, montant_enchere) values (?,?,?,?)";
+	private static final String sqlUpdate = "update ENCHERES set no_utilisateur = ?,no_article = ?,date_enchere = ?,montant_enchere = ?,telephone = ?";
+
+	@Override
+	public void update(Encheres encheres) throws DALException {
+		try(Connection con = JdbcTools.getConnection();
+				PreparedStatement stmt = con.prepareStatement(sqlUpdate, Statement.RETURN_GENERATED_KEYS);){
+			stmt.setInt(1, encheres.getNoUtilisateur());
+			stmt.setInt(2, encheres.getNoArticle());
+			stmt.setDate(3, (Date) encheres.getDateEnchere());
+			stmt.setInt(4, encheres.getMontantEnchere());
+		
+			int nbRows = stmt.executeUpdate();
+			if(nbRows ==1) {
+				ResultSet rs = stmt.getGeneratedKeys();
+				if(rs.next()) {
+					encheres.setNoUtilisateur(rs.getInt(1));}}
+		}
+		catch (SQLException e) {
+			throw new DALException("insert encheres failed - "+ encheres ,e);
+		}
+	}	
+
+	
+	@Override
+	public Encheres selectById(int id) throws DALException {
+		Encheres encheres = null;
+		try (Connection con = JdbcTools.getConnection();
+				PreparedStatement stmt = con.prepareStatement(sqlSelectById);)
+		{
+			stmt.setInt(1,id);
+			ResultSet rs = stmt.executeQuery();
+			if(rs.next()) {
+				encheres = new Encheres(rs.getInt("no_utilisateur"),
+						rs.getInt("no_article"),
+						rs.getDate("date_enchere"),
+						rs.getInt("montant_enchere")
+						);
+				System.out.println(rs.getString(2));
+			}
+		} catch (SQLException e) {
+			throw new DALException("selcetById failde - id = "+ id, e);			
+		}
+		return encheres;
 	}
+	
+	@Override
+	public void insert(Encheres encheres) throws DALException {
+		try(Connection con = JdbcTools.getConnection();
+				PreparedStatement stmt = con.prepareStatement(sqlInsert, Statement.RETURN_GENERATED_KEYS);){
+			stmt.setInt(1, encheres.getNoUtilisateur());
+			stmt.setInt(2, encheres.getNoArticle());
+			stmt.setDate(3, (Date) encheres.getDateEnchere());
+			stmt.setInt(4, encheres.getMontantEnchere());
 
+			int nbRows = stmt.executeUpdate();
+			if(nbRows ==1) {
+				ResultSet rs = stmt.getGeneratedKeys();
+				if(rs.next()) {
+					encheres.setNoArticle(rs.getInt(1));}}
+		}
+		catch (SQLException e) {
+			throw new DALException("insert utilisateur failed - "+ encheres ,e);
+		}
+	}
 }
