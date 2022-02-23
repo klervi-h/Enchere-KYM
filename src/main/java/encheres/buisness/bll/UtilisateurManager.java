881426eaa06ac41538e5b1bd72681a3ba01abde3
klerv <klerv@192.168.0.26>
2022-02-23 15:57:04 +0100

    création select id by pseudo

--- src/main/java/encheres/buisness/bll/UtilisateurManager.java
+++ src/main/java/encheres/buisness/bll/UtilisateurManager.java
@@ -13,48 +13,44 @@ public class UtilisateurManager {
 	public UtilisateurManager() {
 		this.utilisateurDAO=DAOFactory.getUtilisateurDao();
 	}
-	public Utilisateur afficherParId(int id) throws BusinessException
-	{
+	
+	public Utilisateur afficherParId(int id) throws BusinessException{
 		Utilisateur utilisateur = null;
-		
 		BusinessException exception = new BusinessException();
-
 		try {
 			utilisateur = this.utilisateurDAO.selectById(id);
-		} catch (DALException e) 
-		{
-
+		} catch (DALException e){
 			e.printStackTrace();
 		}
 		return utilisateur;
 	}
 	
-	public void update(Utilisateur utilisateur) throws BusinessException
-	{
-		
+	public void update(Utilisateur utilisateur) throws BusinessException{
 		BusinessException exception = new BusinessException();
-
 		try {
 			this.utilisateurDAO.update(utilisateur);
-		} catch (DALException e) 
-		{
-
+		} catch (DALException e){
 			e.printStackTrace();
 		}
 	}
 	
-
-	public void ajouter(Utilisateur utilisateur) throws BusinessException
-	{
+	public void ajouter(Utilisateur utilisateur) throws BusinessException{
 		BusinessException exception = new BusinessException();
-		
-		
-		
 		try {
 			this.utilisateurDAO.insert(utilisateur);
 		} catch (DALException e) {
-			
 			e.printStackTrace();
 		}
+	}
+	
+	public int idParPseudo(String pseudo)throws BusinessException{
+		int id = 0;
+		BusinessException exception = new BusinessException();
+		try {
+			id = this.utilisateurDAO.idSelectByPseudo(pseudo);
+		} catch (DALException e){
+			e.printStackTrace();
+		}
+		return id;
 	}
 }
--- src/main/java/encheres/dal/UtilisateurDAO.java
+++ src/main/java/encheres/dal/UtilisateurDAO.java
@@ -4,16 +4,8 @@ import encheres.buisness.bo.Utilisateur;
 
  public interface UtilisateurDAO {
 	public void update(Utilisateur utilisateur) throws DALException;
-	 
 	public Utilisateur selectById(int id) throws DALException;
-	/*public List<Article> selectAll() throws DALException;
-	public List<Article> selectByMarque(String marque) throws DALException;
-	public List<Article> selectByMotCle(String motCle) throws DALException;
-	public void insert(Article article) throws DALException;
-	public void update(Article article) throws DALException;
-	public void delete(Integer idArt) throws DALException;*/
-
 	public void insert(Utilisateur utilisateur) throws DALException;
-
 	String selectByPassword(String pseudo) throws DALException;
+	public int idSelectByPseudo(String pseudo) throws DALException;
 }
