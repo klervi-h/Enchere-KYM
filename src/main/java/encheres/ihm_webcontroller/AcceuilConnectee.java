55c30d608f9fc6ad660c7e4d12aa358855c05cc0
klerv <klerv@192.168.0.26>
2022-02-24 11:25:54 +0100

    accceuil connecté

---
+++ src/main/java/encheres/ihm_webcontroller/AcceuilConnectee.java
@@ -0,0 +1,47 @@
+package encheres.ihm_webcontroller;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import encheres.buisness.bll.ArticleManager;
+import encheres.buisness.bo.Article;
+
+/**
+ * Servlet implementation class AcceuilConnectee
+ */
+@WebServlet("/AcceuilConnectee")
+public class AcceuilConnectee extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	ArticleManager aM = new ArticleManager();
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		List<Article> listeArticle = null;
+		try {
+			listeArticle = aM.getCatalogue();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}		
+		request.setAttribute("listeArticle", listeArticle);
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
+		rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
+		rd.forward(request, response);
+	}
+
+}
