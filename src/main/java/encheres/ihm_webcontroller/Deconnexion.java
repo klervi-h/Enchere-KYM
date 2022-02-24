377a1a85cdb41b57b9f7b320c9f2f7a9beba8e1f
yohqu <yohqu@DESKTOP-YOHQUI>
2022-02-24 10:54:42 +0100

    servlet de déconnexion à tester un fois le probleme de la page d'accueil sera résolu

---
+++ src/main/java/encheres/ihm_webcontroller/Deconnexion.java
@@ -0,0 +1,43 @@
+package encheres.ihm_webcontroller;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Deconnexion
+ */
+@WebServlet("/Deconnexion")
+public class Deconnexion extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+		
+		
+			HttpSession session = request.getSession(true); 
+			session.invalidate();
+			
+			RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/jsp/accueil.jsp");
+			rd.forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		doGet(request, response);
+	}
+
+}
